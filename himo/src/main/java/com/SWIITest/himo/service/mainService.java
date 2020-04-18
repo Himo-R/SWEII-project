05553be8553daf33090071f487c4eabae10e913a
hammam98 <45409627+hammam98@users.noreply.github.com>
2020-04-18 17:26:50 +0200

    add homeAdmin-homeUser-homPage and configure sequence

--- himo/src/main/java/com/SWIITest/himo/service/mainService.java
+++ himo/src/main/java/com/SWIITest/himo/service/mainService.java
@@ -4,26 +4,40 @@ import com.SWIITest.himo.RepositoryDataAccess;
 import com.SWIITest.himo.model.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @Service
 public class mainService {
     @Autowired
-    private RepositoryDataAccess todoRepository;
+    private RepositoryDataAccess RepositoryDataAccess;
 
 
-    public List<user> findAll() { return todoRepository.findAll(); }
+    public List<user> findAll() { return RepositoryDataAccess.findAll(); }
 
 
     public user userById(String id) {
-       return todoRepository.findById(id).get();
+       return RepositoryDataAccess.findById(id).get();
     }
 
 
     public boolean Registration(user od) {
-        return todoRepository.insert(od)==null?false:true;
+        return RepositoryDataAccess.insert(od)==null?false:true;
     }
+
+
+    public String homeUser(){ return "<center><h1>Wellcome user </h1> <br> <a href='/logout'>logout!</a></center>"; }
+
+    public String homeAdmin(){ return "<center><h1>wellcome Admin <h1> <br> <a href='/listAllUsers'>list all user!</a> <br> <a href='/logout'>logout!</a> </center>"; }
+
+    public String HomePage(){   return
+            "<center><h1>wellcome API<h1> " +
+            "<br> <a href='/UserPage'>USER LOGIN!</a>" +
+            "<br> <a href='/AdminPage'>ADMIN LOGIN!</a></center>"
+            ;}
+
+
 
     /*
     public void remove(String idToRemove) { todoRepository.deleteById(idToRemove);
